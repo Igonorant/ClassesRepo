876c209a8f7de61c4ea569b058d94286e8a1726c
igolino3@hotmail.com <igorsmarques0@gmail.com>
2020-02-06 21:58:22 -0300

    Included memory leak detection in main.cpp using crtdbg

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
-#include <Windows.h>
+// Memory leak detection using NEW keyword instead of new
+#ifdef _DEBUG
+	#define _CRTDBG_MAP_ALLOC
+	#define NEW new( _NORMAL_BLOCK, __FILE__, __LINE__ )
+	#include <stdlib.h>
+	#include <crtdbg.h>
+#else
+	#define NEW new
+#endif
+
+// STD libraries
 #include <iostream>
 #include <math.h>
+
+// Other libraries
 #include "logger.h"
 #include "timer.h"
 
 void main()
 {
+	// Memory leak detection
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+
 	Logger logger("Time (ms)");
 	Timer loopTimer;
 
@@ -16,6 +32,7 @@ void main()
 
 	logger.Log(std::to_string(loopTimer.Mark()), Logger::Type::Custom);
 
+	int* a = NEW int[50];
 
 	for (int i = 0; i < 10000000; i++)
 	{
@@ -25,9 +42,6 @@ void main()
 
 	logger.Log(loopTimer.Mark());
 
-	while (!GetAsyncKeyState(VK_SPACE))
-	{
-	}
-
+	std::cin.get();
 
 }
